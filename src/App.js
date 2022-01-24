ef2d730ff3c98144ceb5befb7e8bb250821d203b
Marc Abillama <mabillama@gmail.com>
2022-01-24 23:58:09 +0100

    Basic styling added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>My app</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
