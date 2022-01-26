7a432c2b00592d1f2d5e88ba98ca9e99b73ec546
Marc Abillama <mabillama@gmail.com>
2022-01-26 20:41:58 +0100

    useContext added. App.js a lot leaner due to data manipulation being transfered to FeedbackContext.js

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import React from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
 import RatingSelect from "./RatingSelect";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackForm({ handleAdd }) {
+function FeedbackForm() {
   const [text, setText] = useState("");
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
+
+  const { addFeedback } = useContext(FeedbackContext);
 
   const handleTextChange = (e) => {
     if (text === "") {
@@ -29,7 +32,7 @@ function FeedbackForm({ handleAdd }) {
     e.preventDefault();
     if (text.trim().length > 10) {
       const newFeedback = { text, rating };
-      handleAdd(newFeedback);
+      addFeedback(newFeedback);
       setText("");
     }
   };
